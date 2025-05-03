1f1835db21e62ce77d26171b24cac2b835f2249b
Parastoo <zhmosayebi@gmail.com>
2025-05-04 00:49:39 +0200

    Fix import path case for FeedbackItem.jsx

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import FeedbackItem from "./Feedbackitem";
+import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) {
